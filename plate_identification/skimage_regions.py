249225448276df2ddc731adaff79711afcfce2b3
Song Li <da_tou_li@yahoo.com>
2018-11-18 01:03:47 -0500

    skimage regions

---
+++ plate_identification/skimage_regions.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from skimage.color import rgb2gray
+
+image_path = "./images/xu.jpeg"
+
+image = imread(image_path)
+g_image = rgb2gray(image)
+
+thresh = threshold_otsu(g_image)
+bw = closing(g_image > thresh, square(3))
+
+cleared = clear_border(bw)
+label_image = label(cleared)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.imshow(image)
+
+regions = regionprops(label_image)
+for region in regions:
+
+    if region.area >= 1500:
+
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr),
+            maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=2)
+        
+        ax.add_patch(rect)
+
+ax.set_axis_off()
+plt.tight_layout()
+
+plt.show()
+
+
+
+
